32ef6ce441c9d7f09da18f3e4cc876176d0f6579
Alexander Alkhimyonok <aleksander.a@playgendary.com>
2018-10-19 02:08:39 +0300

    init project

---
+++ src/main/java/com/buckartz/pvt/trafficjamcontroll/TrafficJamControllApplication.java
@@ -0,0 +1,12 @@
+package com.buckartz.pvt.trafficjamcontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficJamControllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficJamControllApplication.class, args);
+    }
+}
---
+++ src/test/java/com/buckartz/pvt/trafficjamcontroll/TrafficJamControllApplicationTests.java
@@ -0,0 +1,16 @@
+package com.buckartz.pvt.trafficjamcontroll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrafficJamControllApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
